372f3c4984258431ff76fcc90a1f279822dd684f
Maijuriina Mustonen <42972494+maijuriina@users.noreply.github.com>
2022-02-17 10:34:05 +0200

    Latitude and longitude is gotten through useStates

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  // creating latitude and longitude states
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+
+    console.log("Latitude: ", lat)
+    console.log("Longitude: ", long)
+  }, [lat, long]);
+
   return (
     <div className="App">
-      
+      <h1>Weather App</h1>
     </div>
   );
 }
